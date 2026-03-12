73ca1aa0b61999f2270dd31d54d86a577926f169
kappanaveenreddy <106686082+kappanaveenreddy@users.noreply.github.com>
2026-03-12 11:52:18 +0530

    Add MappingProfile file for AutoMapper configurations

---
+++ backend/ProductAPI/Mappings/MappingProfile.cs
@@ -0,0 +1,12 @@
+using AutoMapper;
+
+namespace ProductAPI.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // CreateMap<Source, Destination>();
+        }
+    }
+}
